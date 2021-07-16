26f99e731030c7ffaaa7e2080a7423c859100180
Rocael <rocael@qq.com>
2021-07-17 00:32:59 +0800

    修改：注释

--- source/real-order/src/main/kotlin/edu/jmu/seven/controller/HelloController.kt
+++ source/real-order/src/main/kotlin/edu/jmu/seven/controller/HelloController.kt
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 /**
- * <p></p>
+ * <p>这里是用来做简单Controller测试的类</p>
+ *
  * @author github/blackswords
  * @date 2021/07/16/20:08
  */
