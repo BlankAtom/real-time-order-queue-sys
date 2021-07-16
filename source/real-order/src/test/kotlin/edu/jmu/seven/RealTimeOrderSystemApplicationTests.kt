71d8e3e0af1e0c27ae2078967228844b50681b3f
Rocael <rocael@qq.com>
2021-07-17 00:34:12 +0800

    增加：Account数据库的测试代码

--- source/real-order/src/test/kotlin/edu/jmu/seven/RealTimeOrderSystemApplicationTests.kt
+++ source/real-order/src/test/kotlin/edu/jmu/seven/RealTimeOrderSystemApplicationTests.kt
@@ -1,17 +1,40 @@
 package edu.jmu.seven
 
-import com.baomidou.mybatisplus.generator.AutoGenerator
-import com.baomidou.mybatisplus.generator.config.*
+import edu.jmu.seven.entity.Account
+import edu.jmu.seven.mapper.AccountMapper
+import edu.jmu.seven.utils.SHAUtil
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 
 @SpringBootTest
 class RealTimeOrderSystemApplicationTests {
 
+    @Autowired
+    lateinit var mapper: AccountMapper
+
     @Test
     fun contextLoads() {
+        // 对自动填充、逻辑删除、版本号进行数据库测试
+        // Select All
+        var res = mapper.selectList(null)
+        res.forEach { println(it.toString()) }
 
+        // Insert new, And the sha512's length is least 64
+        val pwd = SHAUtil().SHA512("123456")
+        println("$pwd : ${pwd?.length}")
+        mapper.insert(Account("test_123456", pwd!!, 1))
+
+        res = mapper.selectList(null)
+        res.forEach { println(it.toString()) }
+
+        mapper.deleteById("test_123456")
+
+
+        res = mapper.selectList(null)
+        res.forEach { println(it.toString()) }
     }
 
 }
