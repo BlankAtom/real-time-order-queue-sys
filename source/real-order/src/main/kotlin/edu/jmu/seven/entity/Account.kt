859153d5a52677eba4f7dace00182c213f54af23
Rocael <rocael@qq.com>
2021-07-17 09:41:58 +0800

    增加：Account注释

--- source/real-order/src/main/kotlin/edu/jmu/seven/entity/Account.kt
+++ source/real-order/src/main/kotlin/edu/jmu/seven/entity/Account.kt
@@ -15,28 +15,65 @@ import java.time.LocalDateTime
 
 @TableName("vocation.account")
 class Account(
+    /**
+     * username: 字段，用户名，自动生成策略：user+电话号码
+     */
     @TableId("username")
     private var username: String,
+
+    /**
+     * password: 字段，密码（已加密），可以使用SHA512加密算法（128长度）
+     * 加密算法可以调用 class： SHAUtil
+     */
     private var password: String,
-    var auth_id: Int,
+
+    /**
+     * auth_id: 字段，权限码，1、2、9，用户、商家、管理员
+     */
+    private var auth_id: Int,
 ) : Serializable, UserDetails {
 
-    @TableField(fill = FieldFill.INSERT)
-    var created_at: LocalDateTime = LocalDateTime.now()
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    var updated_at: LocalDateTime = LocalDateTime.now()
+
+    /**
+     * createTime: 字段，创建时间，记录创建记录的最后时间
+     * 不需要开发者进行设定，由MetaHandler进行
+     */
+    @TableField(value = "created_at" ,fill = FieldFill.INSERT)
+    var createTime: LocalDateTime = LocalDateTime.now()
+
+    /**
+     * updateTime: 字段，更新时间，记录更新记录的最后时间
+     * 不需要开发者进行设定，由MetaHandler进行
+     */
+    @TableField(value = "updated_at", fill = FieldFill.INSERT_UPDATE)
+    var updateTime: LocalDateTime = LocalDateTime.now()
+
+
+    /**
+     * deleted: 字段，逻辑删除，表示是否被删除，当为1（删除）的时候不会被搜索到
+     * 不需要开发者进行设定，@TableLogic 会自己完成
+     */
     @TableLogic
     var deleted: Int = 0
+
+    /**
+     * version: 字段，乐观锁，用于插入、更新时进行版本比较
+     * 不需要开发者进行设定，@Version 会自己完成
+     */
     @Version
     var version: Int = 1
 
+    /**
+     * 转为类名+json格式的字符串
+     * @return 全部可现实内容的组合字符串
+     */
     override fun toString(): String {
         return "Account{" +
                 "username=" + username +
                 ", password=" + password +
                 ", auth_id=" + auth_id +
-                ", created_at=" + created_at +
-                ", updated_at=" + updated_at +
+                ", created_at=" + createTime +
+                ", updated_at=" + updateTime +
                 "}"
     }
 
@@ -44,6 +81,10 @@ class Account(
     fun setPassword(s: String) { this.password = s }
 
 
+    /**
+     * 获取权限表，因为本项目的设定是通过数字确定权限，故，返回只含有一个数字的权限集合即可
+     * @return MutableCollection 权限表，可以直接当作List使用
+     */
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
         if( auth_id!=0 ) {
             val gens = ArrayList<GrantedAuthority>()
