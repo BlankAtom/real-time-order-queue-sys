966570afc08d4951a14b4d48cdd74505163edefd
黄小强 <2312352919@qq.com>
2021-07-20 10:53:45 +0800

    修改:简单绑定数据

--- source/real_order_web/src/views/common/Queue.vue
+++ source/real_order_web/src/views/common/Queue.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="outer" style="height: 100%;width: 100%">
         <div class="center">
-            <h1 style="size: auto;margin-top: 50px">俺是个店名</h1>
+            <h1 style="size: auto;margin-top: 50px">{{ mName }}</h1>
         </div>
         <div class="center"
              style="border-radius: 100%;
@@ -10,15 +10,15 @@
             margin-bottom: 50px;
             background-color: lightblue">
             <div style="height: max-content;width: max-content;margin: auto;padding-top: 30%    ">
-                <label style="size: auto;font-size: 50px">太难了</label>
+                <label style="size: auto;font-size: 50px">{{text}}</label>
             </div>
         </div>
         <div class="center">
-            <label style="size: auto;font-size: 50px">前面还有好多人</label>
+            <label style="size: auto;font-size: 50px">前面还有{{ qNub }}人</label>
         </div>
         <div class="center" style="margin-top: 80px">
-            <el-button class="center" type="primary" style="size: 100px;font-size: 60px;background-color: #0c212b">
-                他妈有本事点一下
+            <el-button class="center" type="primary" style="size: 100px;font-size: 60px;background-color: #0c212b" @click="clickChange">
+                {{ btnText }}
             </el-button>
         </div>
 
@@ -28,7 +28,22 @@
 </template>
 
 <script>
-
+    export default {
+        data(){
+            return{
+                mName:'俺是个店名',
+                text:'排队号',
+                qNub:'很多',
+                btnText:'有本事点一下'
+            }
+        },
+        methods:{
+            clickChange(val){
+                this.btnText='你还真点啊'
+                console.log(this.btnText)
+            }
+        }
+    }
 </script>
 
 <style>
