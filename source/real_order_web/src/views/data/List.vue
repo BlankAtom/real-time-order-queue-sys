19e43f3c85388f07fb59ce37a56ed1040a77509c
王道松 <1308487138@qq.com>
2021-07-20 10:57:52 +0800

    修改了订单页面

--- source/real_order_web/src/views/data/List.vue
+++ source/real_order_web/src/views/data/List.vue
@@ -13,7 +13,7 @@
 <!--      </template>-->
 <!--      </div>-->
         <el-table
-            :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+            :data="tableData"
             style="width: 100%">
           <el-table-column
               label="买家名称"
@@ -27,33 +27,38 @@
               label="订单"
               prop="name">
           </el-table-column>
-          <el-table-column
-              align="right">
-<!--            <template slot="header" slot-scope="scope">-->
-<!--              <el-input-->
-<!--                  v-model="search"-->
-<!--                  size="mini"-->
-<!--                  placeholder="输入关键字搜索"/>-->
-<!--            </template>-->
-<!--            <template slot-scope="scope">-->
+          <el-table-column label="操作">
+            <template #default="scope">
               <el-button
                   size="mini"
-                  @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+                  @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
               <el-button
                   size="mini"
                   type="danger"
-                  @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
-<!--            </template>-->
+                  @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            </template>
           </el-table-column>
         </el-table>
+      <div style="padding-top: 20px" align="center">
+        <el-pagination
+          layout="prev, pager, next"
+          page-size="7"
+          hide-on-single-page="true"
+          background
+
+          :total="totalPage"
+          @current-change="handleCurrentChange">
+      </el-pagination>
+      </div>
 
     </el-card>
 </template>
 
 <script >
 export default {
-  data() {
+  data(){
     return {
+      totalPage: 1,
       tableData: [{
         date: '2016-05-02',
         name: '王小虎',
@@ -70,15 +75,68 @@ export default {
         date: '2016-05-03',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      },{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
     }
+  },
+  created() {
+    const thisView = this
+
+    thisView.totalPage= thisView.tableData.length
   },
   methods: {
     handleEdit(index, row) {
       console.log(index, row);
     },
     handleDelete(index, row) {
-      console.log(index, row);
+      console.log(index, row,this.totalPage);
+    },
+    handleCurrentChange(val){
+      this.tableData=[{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      },{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }]
+      // console.log(thisView.totalPage);
     }
   },
 }
