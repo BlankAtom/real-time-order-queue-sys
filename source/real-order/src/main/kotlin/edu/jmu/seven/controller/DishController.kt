be68ac657068b9b47f24723aa17ff0349617dfee
胡宗瑞 <271850540@qq.com>
2021-07-23 16:24:36 +0800

    补充dish DishController.kt DishMapper.kt 补充xgplayer

--- source/real-order/src/main/kotlin/edu/jmu/seven/controller/DishController.kt
+++ source/real-order/src/main/kotlin/edu/jmu/seven/controller/DishController.kt
@@ -1,10 +1,13 @@
 package edu.jmu.seven.controller
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
+import com.baomidou.mybatisplus.core.metadata.IPage
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page
 import edu.jmu.seven.entity.Dish
 import edu.jmu.seven.mapper.DishMapper
 import io.lettuce.core.internal.LettuceStrings.toDouble
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.domain.Page
+
 import org.springframework.data.domain.PageRequest
 import org.springframework.web.bind.annotation.*
 
@@ -14,13 +17,7 @@ import org.springframework.web.bind.annotation.*
 public  class DishController {
     @Autowired
     private lateinit var dishMapper : DishMapper
-//    @GetMapping("/findAll/{page}/{size}")
-//    open fun findAll(@PathVariable("page")  page:Int, @PathVariable("size") size:Int ) : List<Dish>{
-////        println(dishMapper.findAll());
-//
-//        val request :PageRequest= PageRequest.of(page, size)
-//        return dishMapper.findAll(request)
-//    }
+
 
     @GetMapping("/findAll")
     open fun findAll( ) : List<Dish>{
@@ -28,18 +25,15 @@ public  class DishController {
 
         return dishMapper.findAll()
     }
-//    @GetMapping("/findPage")
-//    open  fun  findPage(@PathVariable("page")  page:Int, @PathVariable("size")  size:Int): Page<Dish> {
-//        val request :PageRequest= PageRequest.of(page, size)
-//        return
-//    }
-////    @PutMapping("/insertDish")
-//    open fun insert(str: String):String {
-//
-//        ///println(44444444444444444444444444444444)
-//        return str
-//        //return dishMapper.insert(dish)
-//    }
+
+    @RequestMapping("/findByid")
+    open fun findByid( @RequestParam("merchant_now") m_id: String ) : List<Dish>{
+//        println(dishMapper.findAll());
+        var dishwarapper = QueryWrapper<Dish>().eq("m_id",m_id)
+        return dishMapper.selectList(dishwarapper)
+    }
+
+
     @RequestMapping("/insertDish")
     open fun insert(@RequestParam("d_name") d_name: String,
                     @RequestParam("d_id") d_id: String,
@@ -54,11 +48,65 @@ public  class DishController {
 
                 var double_price=d_price.toDouble()
                 var int_sum=d_sum.toInt()
-                println(d_name+"___"+d_id+"___"+m_id+"___"+
+                println("增加"+d_name+"___"+d_id+"___"+m_id+"___"+
                         d_price+"___"+d_pic+"___"+d_note+
                         "___"+d_sum+"___"+cuision_code)
                         var Add_dish=Dish(d_id,d_name,m_id,double_price,d_pic,d_note,int_sum,cuision_code)
-                      //  dishMapper.insert(Add_dish)
+                        dishMapper.insert(Add_dish)
 
+    }
+
+
+    @RequestMapping("/updateDish")
+   fun update(
+      @RequestParam("d_name") d_name: String,
+      @RequestParam("d_id") d_id: String,
+      @RequestParam("m_id") m_id: String,
+      @RequestParam("d_price") d_price: String,
+      @RequestParam("d_pic") d_pic: String,
+      @RequestParam("d_note") d_note: String,
+      @RequestParam("d_sum") d_sum: String,
+      @RequestParam("cuision_code")  cuision_code: String
+    ){
+        var double_price=d_price.toDouble()
+        var int_sum=d_sum.toInt()
+//        println("更新"+d_name+"___"+d_id+"___"+m_id+"___"+
+//                d_price+"___"+d_pic+"___"+d_note+
+//                "___"+d_sum+"___"+cuision_code)
+        var upda_dish=Dish(d_id,d_name,m_id,double_price,d_pic,d_note,int_sum,cuision_code)
+        //  dishMapper.insert(Add_dish)
+        dishMapper.updateById(upda_dish)
+
+    }
+
+    @RequestMapping("/deleteDish")
+    fun delete(
+        @RequestParam("dish_id") d_id: String
+    ){
+        println("删除 "+d_id)
+        var dishwarapper = QueryWrapper<Dish>().eq("d_id",d_id)
+        dishMapper.delete(dishwarapper)
+//        println(dishMapper.selectList(dishwarapper))
+        //  dishMapper.insert(Add_dish)
+
+    }
+    @RequestMapping("/findPage")
+    fun findPageDish(
+        @RequestParam("current_page") current_page: String,
+        @RequestParam("size_page") size_page: String,
+        @RequestParam("m_id") m_id: String
+
+    ):List<Dish>{
+        var int_page=current_page.toLong()
+        var int_size=size_page.toLong()
+        var dishPage: Page<Dish> = Page<Dish>(int_page, int_size);
+
+        val dishwrapper = QueryWrapper<Dish>().eq("m_id", m_id)
+        var dishIPage: IPage<Dish> = dishMapper.selectPage(dishPage,dishwrapper)
+        val dishList: List<Dish> =dishIPage.records;
+//        orderList.forEach { println(it.toString())}
+//        dishList.forEach { println(it.toString()) }
+        return dishList
+//        Page<Orders>() orderPage = new Page<>(curPage,size);
     }
 }
--- source/real_order_web/src/views/common/XGPlayer.vue
+++ source/real_order_web/src/views/common/XGPlayer.vue
@@ -3,12 +3,12 @@
   <div class="hello">
     <h1>菜品展示</h1>
     <el-row class="table-grid-content">
-      <el-col :span="18" class="grid">
-        <el-input v-model="input" placeholder="请输入搜索内容"></el-input>
-      </el-col>
-      <el-col :span="3" class="grid" :gutter="1">
-        <el-button type="success"  icon="el-icon-search">搜索</el-button>
-      </el-col>
+<!--      <el-col :span="18" class="grid">-->
+<!--        <el-input v-model="input" placeholder="请输入搜索内容"></el-input>-->
+<!--      </el-col>-->
+<!--      <el-col :span="3" class="grid" :gutter="1">-->
+<!--        <el-button type="success"  icon="el-icon-search">搜索</el-button>-->
+<!--      </el-col>-->
       <el-col :span="2" class="grid" :gutter="15">
         <el-button type="primary" @click="addMembers()">增加</el-button>
       </el-col>
@@ -19,14 +19,20 @@
       <el-table-column label="菜名" prop="d_name"></el-table-column>
       <el-table-column label="商家id" prop="m_id"></el-table-column>
       <el-table-column label="价格" prop="d_price"></el-table-column>
-      <el-table-column label="图片" prop="d_pic"></el-table-column>
+      <el-table-column label="图片" prop="d_pic">
+
+      <template   #default="scope">
+        <img :src="scope.row.d_pic"  min-width="70" height="70" />
+      </template>
+
+      </el-table-column>
       <el-table-column label="注意事项" prop="d_note"></el-table-column>
       <el-table-column label="销量" prop="d_sum"></el-table-column>
-      <el-table-column label="菜系" prop="cuision_note"></el-table-column>
+      <el-table-column label="菜系" prop="cuision_code" :formatter="cuisionType"> </el-table-column>
       <el-table-column label="操作">
         <template #default="scope">
           <el-button type="primary" @click="modifyData(scope.$index, scope.row)">修改</el-button>
-          <el-button type="danger" @click="deleteData(scope.$index,tableData)">删除</el-button>
+          <el-button type="danger" @click="deleteData(scope.$index,scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -58,12 +64,6 @@
     </el-dialog>
     <el-dialog v-model="isAddMembers">
       <el-form :model="addForm">
-<!--        <el-form-item label="日期" :picker-options="pickerOptions">-->
-<!--          <el-date-picker v-model="addForm.date" type="date" placeholder="选择日期" value-format="yyyy-MM-dd"></el-date-picker>-->
-<!--        </el-form-item>-->
-<!--        <el-form-item label="菜id">-->
-<!--          <el-input v-model="addForm.d_id"></el-input>-->
-<!--        </el-form-item>-->
         <el-form-item label="菜名">
           <el-input v-model="addForm.d_name"></el-input>
         </el-form-item>
@@ -79,7 +79,6 @@
         <el-form-item label="菜系">
           <el-input v-model="addForm.cuision_code"></el-input>
         </el-form-item>
-
       </el-form>
       <div>
         <el-button @click="closeDialog()">取消</el-button>
@@ -92,7 +91,8 @@
   <el-pagination
       background
       layout="prev, pager, next"
-      :total="100"
+      page-size="9"
+      :total="totalPage"
       @current-change="page">
   </el-pagination>
 
@@ -108,18 +108,19 @@ import {ElMessage} from "element-plus";
 
 
  var _index;
+ var total_size;
  var  merchant_now='merchant_8995566'  //等待商家id传入
 export default {
-
   data() {
-
     return {
-
+      totalPage: 7,
       // msg: 'Welcome to Your Vue.js App',
       centerDialogVisible: false,
       isAddMembers: false,
       editForm: [],
       addForm: [],
+      deleteForm:[],
+      searchForm:[],
       searchData: '',
       input: '',
       id:'',
@@ -136,7 +137,7 @@ export default {
         d_pic: '',
         d_note: '',
         d_sum: '',
-        cuision_code:''
+        cuision_code: ''
       }
 
       ],
@@ -144,44 +145,85 @@ export default {
     }
   },
   created (){
-
+    let search_params = {merchant_now};
     const _this = this
-    axios.get('http://localhost:8080/dish/findAll').then(function(resp){
-      console.log(resp.data)
-      _this.tableData = resp.data
-      _this.pageSize = resp.data.size
-      _this.total = resp.data.total
+    this.$axios
+        .post("/dish/findByid", this.$qs.stringify(search_params))
+        .then(res => {
+          _this.searchForm = res.data
+          total_size=res.data.length
+          this.totalPage=res.data.length
+         // console.log(this.totalPage+"测试/////////////////")
+          // console.log("搜索 数据加载ssssuucccceeeesssss!!!!"+total_size)
+        })
+        .catch(err => {
+          console.log("///////////////////////报错", err);
 
-     // console.log("总页数"+_this.pageSize+"      总条数  "+_this.total)
-    })
-    // const {proxy} = getCurrentInstance();
-    // proxy.$axios
-    //     .post('/dish/findAll')
-    //     .then(res => {
-    //
-    //       console.log(res)
-    //     })
-    //     .catch(err => {
-    //       console.log("login err", err);
-    //
-    //     });
+        })
+
+    let params = {
+      current_page:1,
+      size_page: 9,
+      m_id:merchant_now
+      };
+    this.$axios
+        .post("/dish/findPage", this.$qs.stringify(params))
+        .then(res => {
+
+          _this.tableData = res.data
+          //_this.total=total_size
+         // console.log("数据加载ssssuucccceeeesssss!!!!")
+        })
+        .catch(err => {
+          console.log("///////////////////////报错", err);
+
+        })
+    console.log(this.totalPage)
   },
   methods: {
+    page(currentPage){
+     const _this = this
+      let params = {
+        current_page:currentPage,
+        size_page: 9,
+        m_id:merchant_now
+      };
+      this.$axios
+          .post("/dish/findPage", this.$qs.stringify(params))
+          .then(res => {
 
-    // page(currentPage){
-    //   const _this = this
-    //   axios.get('http://localhost:8080/dish/findAll/'+(currentPage-1)+'/6').then(function(resp){
-    //     console.log(resp)
-    //     _this.tableData = resp.data.content
-    //     _this.pageSize = resp.data.size
-    //     _this.total = resp.data.total
-    //   })
-    // },
+            _this.tableData = res.data
+           // _this.total=total_size
+           // console.log("数据加载ssssuucccceeeesssss!!!!")
+          })
+          .catch(err => {
+            console.log("///////////////////////报错", err);
+
+          })
+      console.log(this.totalPage)
+
+    },
     deleteData(index, row) {
+      this.deleteForm = Object.assign({}, row); //重置对象
+
       this.tableData.splice(index, 1)
       console.log("进行了删除操作")
-      console.log("index的值是：" + index)
-      console.log("row的值是：", row)
+     //console.log("index的值是：" + index)
+      console.log("该菜品的id为：", this.deleteForm.d_id)
+
+      var dish_id=this.deleteForm.d_id
+      let params ={dish_id}
+      this.$axios
+          .post("/dish/deleteDish", this.$qs.stringify(params))
+          .then(res => {
+
+
+            // console.log("数据加载ssssuucccceeeesssss!!!!")
+          })
+          .catch(err => {
+            console.log("///////////////////////报错", err);
+
+          })
     },
     modifyData(index, row) {
       this.centerDialogVisible= true
@@ -206,12 +248,34 @@ export default {
       this.centerDialogVisible = false;
      // console.log("数据：" + this.editForm.d_name)
      // console.log("对象数组", this.tableData)
+      this.editForm.d_sum=0
+      let params = {
+        d_name:this.editForm.d_name,
+        d_id: this.editForm.d_id,
+        m_id:merchant_now,
+        d_price: this.editForm.d_price,
+        d_pic: this.editForm.d_pic,
+        d_note:this.editForm.d_note,
+        d_sum: this.editForm.d_sum,
+        cuision_code: this.editForm.cuision_code};
+      // const _this = this
+      this.$axios
+          .post("/dish/updateDish", this.$qs.stringify(params))
+          .then(res => {
+            //let {code, data, msg, success} = res.data;
+
+            // console.log("更新ssssuucccceeeesssss!!!!")
+          })
+          .catch(err => {
+            console.log("更新///////////////////////报错", err);
+
+          });
     },
     closeDialog() {
       this.centerDialogVisible = false
       this.isAddMembers = false
-      console.log( this.editForm)
-      console.log( this.addForm)
+    //  console.log( this.editForm)
+     // console.log( this.addForm)
 
     },
     addMembers() {
@@ -227,7 +291,7 @@ export default {
         cuision_code:''
       }
 
-      console.log(this.addForm)
+    //  console.log(this.addForm)
     },
     sumbitAddRow() {
 
@@ -238,8 +302,11 @@ export default {
       // console.log("表格是:" +monthNow )
       this.addForm.d_sum=0
       this.addForm.d_id="dish_"+
-          this.tableData[0].m_id.substring(this.tableData[0].m_id.length-4,this.tableData[0].m_id.length)+
-          "_"+monthNow+"_"+nowDate.getDate()+"_"+getRandomInt(100)
+          merchant_now.substring(merchant_now.length-4,merchant_now.length)+
+          "_"+monthNow+nowDate.getDate()+"_"+getRandomInt(99)
+      // this.addForm.d_id="dish_"+
+      //     merchant_now.substring(merchant_now.length-4,merchant_now.length)
+      //     +"_"+getRandomInt(99)
       this.tableData.push({
         d_id: this.addForm.d_id,
         d_name: this.addForm.d_name,
@@ -268,49 +335,48 @@ export default {
           .then(res => {
             //let {code, data, msg, success} = res.data;
 
-            console.log("ssssuucccceeeesssss!!!!")
+            // console.log("ssssuucccceeeesssss!!!!")
           })
           .catch(err => {
         console.log("///////////////////////报错", err);
 
       });
 
-      //var str =JSON.stringify(this.addForm)
-      // axios.put('http://localhost:8080/dish/insertDish',params).then(function(resp){
-      //   console.log(resp.data)
-      //   console.log(111111111111111111111111111111111111111111111111)
-      // })
+
+
+    },
+    cuisionType:function (row,column,cellValue){
+      if(cellValue==="1"){
+        return "菜系1"
+      }
+      else if(cellValue==="2")
+        return "菜系2"
+      else if(cellValue==="3")
+        return "菜系3"
+      else if(cellValue==="4")
+        return "菜系4"
+      else if(cellValue==="5")
+        return "菜系5"
+
 
     }
-    // searchMembers(){
-    //   const input = this.input
-    //   if (input) {
-    //     console.log("input输入的搜索内容：" + this.input)
-    //     return this.tableData.filter(data => {
-    //       console.log("object:" + Object.keys(data.d_name))
-    //       return Object.keys(data).some(key => {
-    //         return String(data[key]).toLowerCase().indexOf(input) > -1
-    //       })
-    //     })
-    //   }
-    //   return this.tableData
-    // }
+
   },
-  computed: {
-    tableData() {
-      const input = this.input
-      if (input) {
-        console.log("input输入的搜索内容：" + this.input)
-        return this.tableData.filter(data => {
-          console.log("object:" + Object.keys(data))
-          return Object.keys(data).some(key => {
-            return String(data[key]).toLowerCase().indexOf(input) > -1
-          })
-        })
-      }
-      return this.tableData
-    }
-  },
+  // computed: {
+  //   tableData() {
+  //     const input = this.input
+  //     if (input) {
+  //       console.log("input输入的搜索内容：" + this.input)
+  //       return this.searchForm.filter(data => {
+  //         console.log("object:" + Object.keys(data))
+  //         return Object.keys(data).some(key => {
+  //           return String(data[key]).toLowerCase().indexOf(input) > -1
+  //         })
+  //       })
+  //     }
+  //     return this.tableData
+  //   }
+  // },
 
 }
 </script>
